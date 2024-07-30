ad4efb1f30f87eaef313e7f0e7a2d55a2ec53aad
Sergey <sonot190000@gmail.com>
2024-07-30 17:33:34 +0200

    aga

---
+++ test1.py
@@ -0,0 +1,32 @@
+a = 10
+b = 10 
+Math = 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+def math(Math):
+    if Math == 1:
+        return c
+    if Math == 2:
+        return d
+    if Math == 3:
+        return e
+    if Math == 4:
+        return f   
+
+print(math(Math))
