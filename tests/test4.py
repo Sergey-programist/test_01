e1596b7d78300283c11df7ab384070b51bd350b3
Sergey <sonot190000@gmail.com>
2024-08-01 11:05:57 +0200

    i make a game

--- tests/test4.py
+++ tests/test4.py
@@ -1,1 +1,51 @@
+from random import randint
 
+
+def greet() -> None:
+    print("Привет, давай ты будешь моим калькулятором? (это игра)\n")
+
+
+def play(score: int) -> int:
+    a = randint(2, 200)
+    b = randint(2, 100)
+    operations = ("+", "-", "*", "/")
+
+    selected_operation = randint(0, 3)
+    question = (f"{a} {operations[selected_operation]} {b}")
+
+    print(question)
+
+    if operations[selected_operation] == "+":
+        correct_ans = a + b 
+        
+    elif operations[selected_operation] == "-":
+        correct_ans = a - b 
+        
+    elif operations[selected_operation] == "*":
+        correct_ans = a * b 
+        
+    elif operations[selected_operation] == "/":
+        correct_ans = a / b 
+        
+    user_ans = float(input())
+
+    if user_ans == correct_ans:
+        score += 1
+        print("Правильно!")
+    else:
+        print("Неправильно!")
+
+    return score
+
+
+def print_current_score(score: int) -> None:
+    print(f"Твой счёт: {score}\n")
+
+
+if __name__ == '__main__':
+    score = 0
+    greet()
+    while True:
+        score = play(score) 
+        print_current_score(score)
+        
